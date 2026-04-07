25021e1c0a23aff6bf0c04dc915f1189168f2ce4
wangdinghui <153421240@qq.com>
2026-04-07 18:17:31 +0800

    提交配置错误修改

--- astro.config.ts
+++ astro.config.ts
@@ -1,25 +1,24 @@
-// @ts-nocheck
-import tailwind from '@astrojs/tailwind';
-import path from 'node:path';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind";
+import path from "node:path";
+
 
 // https://astro.build/config
-export default {
-  site: 'https://grow-kit.com',
-  integrations: [
-    tailwind(),
-  ],
+export default defineConfig({
+  site: "https://grow-kit.com",
+  integrations: [tailwind()],
   // 必须是 static（默认就是这个，删掉多余配置）
   output: "static",
   vite: {
     resolve: {
       alias: {
-        '@layouts': path.resolve('src/layouts'),
-        '@components': path.resolve('src/components'),
-        '@data': path.resolve('src/data'),
-        '@styles': path.resolve('src/styles'),
-        '@utils': path.resolve('src/utils'),
-        '@assets': path.resolve('src/assets'),
-      }
-    }
-  }
-};
+        "@layouts": path.resolve("src/layouts"),
+        "@components": path.resolve("src/components"),
+        "@data": path.resolve("src/data"),
+        "@styles": path.resolve("src/styles"),
+        "@utils": path.resolve("src/utils"),
+        "@assets": path.resolve("src/assets"),
+      },
+    },
+  },
+});
